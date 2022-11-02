06077a7b480a6a2bb9bd37dace66a18eb8c271ce
olivier3 <olivierbilodeau@outlook.com>
2022-11-02 15:19:45 -0400

    fix: log in firebase + update log in / off state

--- app/src/main/java/cstjean/mobile/tp2/LoginFragment.kt
+++ app/src/main/java/cstjean/mobile/tp2/LoginFragment.kt
@@ -1,12 +1,18 @@
 package cstjean.mobile.tp2
 
+import android.app.Activity.RESULT_OK
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 import cstjean.mobile.tp2.databinding.FragmentLoginBinding
+import kotlin.math.log
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
@@ -14,6 +20,13 @@ class LoginFragment : Fragment() {
         get() = checkNotNull(_binding) {
             "Binding est null. La vue est visible ??"
         }
+    private var user =  FirebaseAuth.getInstance().currentUser
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,9 +41,32 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
+            if(user == null) {
+                btnConnexion.setText(R.string.connexion)
+            } else {
+                btnConnexion.setText(R.string.deconnexion)
+            }
 
             btnConnexion.setOnClickListener {
-                Log.i("TAG-BTN", "Btn connexion clicked")
+                if (user == null) {
+
+                    // Choose authentication providers
+                    val providers = arrayListOf(
+                        AuthUI.IdpConfig.EmailBuilder().build(),
+                        AuthUI.IdpConfig.GoogleBuilder().build())
+
+                    // Create and launch sign-in intent
+                    val signInIntent = AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build()
+
+                    signInLauncher.launch(signInIntent)
+                } else {
+                    btnConnexion.setText(R.string.connexion)
+                    context?.let { it1 -> AuthUI.getInstance().signOut(it1) }
+                    user = null
+                }
             }
         }
     }
@@ -38,5 +74,20 @@ class LoginFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            binding.btnConnexion.setText(R.string.deconnexion)
+            user = FirebaseAuth.getInstance().currentUser
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+            Log.e("TAG-ERROR", response?.error?.errorCode.toString())
+        }
     }
 }
